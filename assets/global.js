586407cfff90630f044c08423290f5a75173d234
omniscientbsg <omniscientbsg13@gmail.com>
2024-03-10 14:26:26 +0530

    console log

--- assets/global.js
+++ assets/global.js
@@ -970,6 +970,7 @@ class VariantSelects extends HTMLElement {
       this.updateVariantInput();
       this.renderProductInfo();
       this.updateShareUrl();
+      console.log("Variant Has Changed");
     }
   }
 
